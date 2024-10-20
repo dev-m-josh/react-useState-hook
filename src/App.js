95cb217351c61697a4739026dd1201dc386c7330
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-10-20 19:54:31 +0300

    useState intro

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+//rules while using hooks
+//used in function components only
+//hooks must be executed in the same order
+
+function countMultiple() {
+  console.log("Run once!");
+  return 10
+}
+
+function App() {
+  //anything passed inside the useState will be the default value
+  //the first value is the current state while the second value is the function to update the current state
+  //the function passed in the useState hook will be called once
+
+  const [count, setCount] = useState(()=>{
+    return 10
+  });
+
+  function decrementCount() {
+    //setCount(count - 1)
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function increamentCount(){
+    //setCount(count + 1)
+    setCount(currentCount => currentCount + 1)
+  }
+
+  return (
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={increamentCount}>+</button>
+    </>
+  );
+}
+
+export default App;
